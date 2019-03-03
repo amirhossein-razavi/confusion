57c3ef7bccc815909f290f4f4b124f0966ee9c0a
amirhossein razavi <amirhosseinrazavi_76@yahoo.com>
2019-03-03 11:00:46 +0330

    React Router

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -16,9 +16,11 @@ class App extends Component {
   }
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+      </BrowserRouter>
     );
   }
 }
