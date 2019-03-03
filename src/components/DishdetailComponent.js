559934004259f6a65a699e5d0a5b249341681691
amirhossein razavi <amirhosseinrazavi_76@yahoo.com>
2019-03-03 12:50:47 +0330

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 //import { Media } from 'reactstrap';
 
-import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle
-} from 'reactstrap';
+
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 function RenderDish({ dish }) {
@@ -50,36 +50,29 @@ function RenderComments({ comments }) {
 }
 
 const DishDetail = (props) => {
-    if (props.dish != null)
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-6 col-md-5 m-1">
+    return (
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
 
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    {(() => {
-                        if (props.dish.comments != null)
-                            return (
-                                <div className="col-6 col-md-5 m-1">
-
-                                    <RenderComments comments={props.dish.comments} />
-                                </div>
-                            );
-                        else
-                            return (
-                                <div></div>
-                            );
-                    })()}
-
-
-                </div>
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
             </div>
-        );
-    else
-        return (
-            <div></div>
-        );
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
+        </div>
+    );
 }
 
 
